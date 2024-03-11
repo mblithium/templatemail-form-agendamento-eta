7f666ea26e5e8e6c7a70a3a1c4368366ba681529
Matheus Bastos <matheusmcy1@gmail.com>
2024-03-10 23:56:26 -0300

    refactor: new form handling

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const ModAgForm = document.querySelector("form[name=TI_scheduling]");
 
+let Form = {};
+
 const formRef = {
     "name": ModAgForm.querySelector("#formTeacherName"),
     "course": ModAgForm.querySelector("#formCurso"),
@@ -16,14 +18,14 @@ const formRef = {
 const genBtn = ModAgForm.querySelector("#genTemplate");
 genBtn.addEventListener("click", (e) => {
     e.preventDefault();
+
+    Form = new FormData(ModAgForm);
+    Form = Object.fromEntries(Form);
+    Form["Curso"] = getCourseByID(Form["Curso"]);
+
+    console.log(Form);
     genTemplate();
 })
-
-const clearBtn = ModAgForm.querySelector("#clearForm");
-clearBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    clearForm();
-});
 
 const modal = document.querySelector("#modalResult");
 modal.querySelector("button.modalCloseBtn")
@@ -38,54 +40,64 @@ function modalOpenClose() {
 function genTemplate() {
 
     const template = `
-    Professor: ${formRef.name.value}
-    Curso: ${getCourseByID(formRef.course.value)}
-    Sala: ${formRef.classroom.value}
-    Data: ${formRef.schenduling[0].querySelector(".selectDate").value}
-    Horário: ${formRef.schenduling[0].querySelector(".FirstPeriodTime").value} - ${formRef.schenduling[0].querySelector(".SecondPeriodTime").value}
+    Professor: ${Form["Professor"] ? Form["Professor"] : "[ Nenhum nome foi inserido ]"}
+    Curso: ${Form["Curso"] ? Form["Curso"] : "[ Nenhum curso foi selecionado ]"}
+    Sala: ${Form["Sala"] ? Form["Sala"] : "[ Nenhuma sala foi selecionada ]"}
+    Data: ${Form["Data"] ? Form["Data"] : "[ Nenhuma data foi especificada ]"}
+    Horário: ${Form["Hora Início"] ? Form["Hora Início"] : " " } - ${Form["Hora Fim"] ? Form["Hora Fim"] : " " }
 
-    Agendamento de Equipamentos
+    ${Form["Projetor"] || Form["Tela"] || Form["Notebook"] || Form["Caixa de Som"] ? "Agendamento de Equipamentos" : ""}
 
-    ${formRef.equipment.projector.checked ? "- Projetor\n" : ""} ${formRef.equipment.projScreen.checked ? "- Tela\n" : ""} ${formRef.equipment.notebook.checked ? "- Notebook\n" : ""} ${formRef.equipment.sound.checked ? "- Caixa de Som\n" : ""}
-    
-    
+    ${Form["Projetor"] ? "- Projetor\n" : ""} ${Form["Tela"] ? "- Tela\n" : ""} ${Form["Notebook"] ? "- Notebook\n" : ""} ${Form["Caixa de Som"] ? "- Caixa de Som\n" : ""}
+
     `;
 
     modalOpenClose();
     document.querySelector("#resultTemplate").innerText = template;
 }
 
-function clearForm() {
-    let equipment = formRef.equipment;
-    equipment.projector.checked = false;
-    equipment.notebook.checked = false;
-    equipment.sound.checked = false;
-    equipment.projScreen.checked = false;
-    
-    formRef.name.value = "";
-    formRef.course.value = "";
-    formRef.classroom.value = "";
-    formRef.schenduling.forEach((elem) => {
-        elem.querySelector(".selectDate").value = "";
-        elem.querySelector(".FirstPeriodTime").value = "";
-        elem.querySelector(".SecondPeriodTime").value = "";
-    });
-}
-
+// TODO: Refatorar código usando Lookup Table
 function getCourseByID(id) {
     let course = "";
     switch (id) {
-        case "F1A1":
-            course = "Fundamental 1 - 1º Ano"
+        case "F1A1A":
+            course = "Fundamental 1 - 1º Ano A"
             break
-        case "F1A2":
-            course = "Fundamental 1 - 2° Ano"
+        case "F1A1B":
+            course = "Fundamental 1 - 1º Ano B"
             break
-        case "F1A3":
-            course = "Fundamental 1 - 3° Ano"
+        case "F1A1C":
+            course = "Fundamental 1 - 1º Ano C"
             break
-        case "F1A4":
-            course = "Fundamental 1 - 4° Ano"
+        case "F1A1D":
+            course = "Fundamental 1 - 1º Ano D"
+            break
+        case "F1A2A":
+            course = "Fundamental 1 - 2° Ano A"
+            break
+        case "F1A2B":
+            course = "Fundamental 1 - 2° Ano B"
+            break
+        case "F1A2C":
+            course = "Fundamental 1 - 2° Ano C"
+            break
+        case "F1A3A":
+            course = "Fundamental 1 - 3° Ano A"
+            break
+        case "F1A3B":
+            course = "Fundamental 1 - 3° Ano B"
+            break
+        case "F1A3C":
+            course = "Fundamental 1 - 3° Ano C"
+            break
+        case "F1A3D":
+            course = "Fundamental 1 - 3° Ano D"
+            break
+        case "F1A4A":
+            course = "Fundamental 1 - 4° Ano A"
+            break
+        case "F1A4B":
+            course = "Fundamental 1 - 4º Ano B"
             break
         case "F1A5":
             course = "Fundamental 1 - 5° Ano"
